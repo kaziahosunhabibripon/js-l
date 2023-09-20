6e83c916ce9f6b74949f32ee370227befa39d6ec
Kazi Ahosun Habib <34180396+kaziahosunhabibripon@users.noreply.github.com>
2023-09-20 18:30:44 +0000

    truethy false

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-// function
-(function chai(){
-    // named iife
-    console.log(`DB Connected`);
-})();
+// controls flow
+const userEmail = []
+if(userEmail){
+    console.log("Got user email");
+} else{
+    console.log("Don't have user email");
+}
 
-((name)=>{
-    console.log(`Db connected two ${name}`);
-})('ripon');
+// false values 
+// false , 0, -0, BigInt 0n, "", null, undefined, NaN
+// truthe
+// "0", 'false', " ", [], {}, function(){} 
+
+if(userEmail.length === 0){
+    console.log("Array is empty")
+}
+const emptyObj = {}
+if(Object.keys(emptyObj).length===0){
+    console.log("Object is empty");
+}
