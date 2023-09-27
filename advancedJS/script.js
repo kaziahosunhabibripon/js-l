dbe021f6fe527bb0397169e156c86d84fed9bce4
Kazi Ahosun Habib <34180396+kaziahosunhabibripon@users.noreply.github.com>
2023-09-27 14:43:26 +0000

    fetch

--- advancedJS/script.js
+++ advancedJS/script.js
@@ -1,27 +1,22 @@
-const promiseOne = new Promise(function (resolve, _reject){
-    setTimeout(function(){  
-        let error = true
-        if(!error){
-            resolve({
-                userName: "habi",
-                email:"habiib@examp.com",
-                password: 123456
-            })
-        }else{
-            _reject("Error Occure")
-        }
-    }, 1000)
+// async function getAllusers(){
+//     try{
+//         const response  = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     }catch(err){
+//         console.log(err);
+//     }
+// }
+
+// getAllusers()
+
+fetch('https://api.github.com/users/kaziahosunhabibripon')
+.then((res)=>{
+    return res.json()
 })
-.then((user)=>{
-    console.log(user);
-    return user.userName
-})
-.then((userName)=>{
-    console.log(userName);
+.then((data)=>{
+    console.log(data);
 })
 .catch((err)=>{
     console.log(err);
-})
-.finally(()=>{
-    console.log("the promise is resolve or reject");
 })
