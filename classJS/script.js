d16e581b535b304762d1017b61166360777023ae
Kazi Ahosun Habib <34180396+kaziahosunhabibripon@users.noreply.github.com>
2023-09-27 18:00:42 +0000

    analog Clock

--- classJS/script.js
+++ classJS/script.js
@@ -1,8 +1,18 @@
 const user = {
     username: "Ripon",
     loginCount: 8,
-    signedIn: true
+    signedIn: true,
+
+    getUserDetails: function(){
+        // console.log(`UserName: ${this.userName}`);
+        console.log(this);
+    }
 }
 
 
 console.log(user.username);
+// console.log(user.getUserDetails());
+console.log(this);
+
+const promiseOne = new Promise()
+const date = new Date()
